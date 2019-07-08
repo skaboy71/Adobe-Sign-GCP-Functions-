d06ca79f322f7049591787bdf98530e937919f67
Aaron Smusz <skaboy71@gmail.com>
2019-07-07 19:25:52 -0700

    Update getCsvJSON.py

--- getCsvJSON.py
+++ getCsvJSON.py
@@ -5,9 +5,20 @@ import json
 
 def get_csv_json(request):
     """
-    This function uses the data passed in an HTTP post.
+    This function uses the data passed in an HTTP post to the google function endpoint.
+    Example JSON Body for POST:
+    
+    {
+	
+	"shard": "na2",
+	"token": "3AAABLblqZhD7gPDMJ5vjNsKg*** Your API Token Here ***2WqAsaG1oYtllVLaHv9e",
+	"ag_id": "CBJCHBCAAB*** Your V5 or V6 agreement ID here ***hgcVEyj-oI",
+	"s_email": "senders-email@yourdomain.com"
+	
+    }
+    
     It makes the call to the Adobe Sign API V6 using the python "requests" module to get the form data as CSV.
-    It then converts the csv formatted string to JSON and returns it.
+    It then converts the csv formatted string to JSON using the pandas and JSON libraries and returns it.
     This was initially done for use in Microsoft Flow, but it could be used anywhere that can ingest the form data as JSON.
     """
     request_json = request.get_json()
